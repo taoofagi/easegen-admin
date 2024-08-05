ff70bd6bf7d8ebc9a251304898197819f655bab3
chenxin <13227751762@163.com>
2024-08-05 14:26:14 +0800

    fix:ppt合并和ppt解析方法

--- yudao-module-digitalcourse/yudao-module-digitalcourse-biz/src/main/java/cn/iocoder/yudao/module/digitalcourse/service/courseppts/CoursePptsService.java
+++ yudao-module-digitalcourse/yudao-module-digitalcourse-biz/src/main/java/cn/iocoder/yudao/module/digitalcourse/service/courseppts/CoursePptsService.java
@@ -8,6 +8,8 @@ import cn.iocoder.yudao.module.digitalcourse.controller.admin.courseppts.vo.AppC
 import cn.iocoder.yudao.module.digitalcourse.dal.dataobject.courseppts.CoursePptsDO;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 存储课程的PPT信息，包括文件名、文件大小、类型等 Service 接口
@@ -55,5 +57,9 @@ public interface CoursePptsService {
     PageResult<CoursePptsDO> getCoursePptsPage(AppCoursePptsPageReqVO pageReqVO);
 
     CommonResult getSchedule(Long id);
+
+    ResponseEntity<Object> mergePPTs(MultipartFile[] files);
+
+    ResponseEntity<Map<String, Object>> convertPptToImages(MultipartFile file);
 
 }
