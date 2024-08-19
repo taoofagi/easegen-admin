19d577d348ee6611dd49bcfdcdcfc24617c7b9f0
xw88 <1438667965@qq.com>
2024-08-19 15:15:09 +0800

    视频合成,数据回刷

--- yudao-module-digitalcourse/yudao-module-digitalcourse-biz/src/main/java/cn/iocoder/yudao/module/digitalcourse/job/CourseMediaMegerJob.java
+++ yudao-module-digitalcourse/yudao-module-digitalcourse-biz/src/main/java/cn/iocoder/yudao/module/digitalcourse/job/CourseMediaMegerJob.java
@@ -1,6 +1,7 @@
 package cn.iocoder.yudao.module.digitalcourse.job;
 
 import cn.iocoder.yudao.framework.quartz.core.handler.JobHandler;
+import cn.iocoder.yudao.framework.tenant.core.job.TenantJob;
 import cn.iocoder.yudao.module.digitalcourse.service.coursemedia.CourseMediaService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ public class CourseMediaMegerJob implements JobHandler {
     @Resource
     private CourseMediaService courseMediaService;
     @Override
+    @TenantJob
     public String execute(String param) throws Exception {
         courseMediaService.queryRemoteMegerResult();
         return "";
